715a6821511315078903a2a0c68451d0bd48ba2d
leatherbullet <al.obnizov@yandex.ru>
2024-03-03 10:33:37 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int minValue = 0;
+            int maxValue = 101;
+            int number = 2;
+            int numberDegree = 2;
+            int degree = 0;
+            int randomNumber = random.Next(minValue, maxValue);
+            Console.WriteLine("случайное число " + randomNumber);
+
+            while (randomNumber > numberDegree)
+            {
+                degree++;
+                numberDegree *= number;
+            }
+
+            Console.WriteLine($"минимальная степень:{degree} числа:{number}");
+        }
+    }
+}
